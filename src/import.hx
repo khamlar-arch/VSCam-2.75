9e3664f913283d00ea32b184008a1d5e54126ec7
khamlar-arch <khamlar8527@gmail.com>
2026-08-30 05:09:32 +0700

    Update import.hx

--- src/import.hx
+++ src/import.hx
@@ -40,6 +40,22 @@ import funkin.backend.audio.FlxAudio;
 import funkin.backend.LanguageHandler._t;
 import funkin.backend.LanguageHandler._formatT;
 
+// mobile
+import funkin.mobile.backend.StorageUtil;
+
+//Android
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
 using StringTools;
 #end
 
